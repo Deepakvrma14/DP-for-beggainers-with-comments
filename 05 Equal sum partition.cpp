0d30fb0b265a87e9cd05cb36b5cd2aa1a59f2d3d
Deepakvrma14 <deepakverma1482003@gmail.com>
2023-09-13 15:30:22 +0530

    changes

--- 05 Equal sum partition.cpp
+++ 05 Equal sum partition.cpp
@@ -16,6 +16,8 @@ bool isSubsetPossible(int arr[], int n, int sum) {
 				t[i][j] = true;
 		}
 	}
+
+	
 	
 // start from 1 since 1st row and column is already considerd 
 	for (int i = 1; i <= n; i++) {
