a1c4892ce808e475c93d4caa828803055ed7aaee
Deepakvrma14 <deepakverma1482003@gmail.com>
2023-09-22 02:45:12 +0530

    sag

--- 20 Minimum insertion deletion to convert a to b.cpp
+++ 20 Minimum insertion deletion to convert a to b.cpp
@@ -33,3 +33,36 @@ int main() {
 	MinInsertDel(X, Y, n, m) << endl;
 	return 0;
 }
+
+
+
+int minOperations(string s1, string s2) 
+	{ 
+	    // Your code goes here
+	    int n = s1.length();
+	    int m = s2.length();
+	    int t[n+1][m+1];
+	    for(int i =0;i<=n;i++){
+	        for(int j =0;j<=m;j++){
+	            if(i==0 || j==0 ){
+	                t[i][j] = 0;
+	                
+	            }
+	        }
+	    }
+	    for(int i=1;i<=n;i++){
+	        for(int j=1;j<=m;j++){
+	            if(s1[i-1] == s2[j-1]){
+	                t[i][j] = 1+t[i-1][j-1];
+	            }
+	            else{
+	                t[i][j] = max(t[i-1][j] , t[i][j-1]);
+	            }
+	        }
+	    }
+	    int insert = m-t[n][m];
+	    int del = n-t[n][m];
+	    
+	    return insert+del;
+	    
+	} 
